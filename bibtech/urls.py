14f8c2ad0c8a391cbd0a142207af0abdae6a63e1
menintel <menintel@tuta.io>
2025-05-13 00:28:24 +0100

    removed old apps folder and cleaned login screen

--- bibtech/urls.py
+++ bibtech/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-<<<<<<< HEAD
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -32,10 +31,3 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-=======
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
->>>>>>> e1936e36113eaff7b78e011e0caa98a75778e8cf
